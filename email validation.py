6276f4e55d0fb4ad0e6c10537eae1fd61bfe2e07
ashimbabu <ashimbabu@gmail.com>
2026-02-27 20:31:09 +0545

    email validation

---
+++ email validation.py
@@ -0,0 +1,10 @@
+import re
+
+email_pattern = r'^[A-Za-z0-9]+[._]?[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]{2,}$'
+
+user_email = input("Enter your Email: ")
+
+if re.fullmatch(email_pattern, user_email):
+    print("Valid Email")
+else:
+    print("Invalid Email")
